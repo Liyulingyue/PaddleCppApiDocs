a380b4252c58f37dde25f4118fa5a7b925e45ea9
Liyulingyue <852433440@qq.com>
2023-06-12 23:30:11 +0800

    add files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'PaddleCppAPI'
+copyright = '2023, Liyulingyue'
+author = 'Liyulingyue'
 
 release = '0.1'
 version = '0.1.0'
